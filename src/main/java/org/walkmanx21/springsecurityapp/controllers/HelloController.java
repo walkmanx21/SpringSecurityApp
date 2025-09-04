6965d87a11cd88e4a478a43075db05694274b6a3
walkmanx21 <walkmanx21@gmail.com>
2025-09-04 17:44:03 +0300

    ухожу с работы 04.09.2025

---
+++ src/main/java/org/walkmanx21/springsecurityapp/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package org.walkmanx21.springsecurityapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+}
